c9dc6dc204b10edab559703fc551926366006794
SendiSteve <sendi.stev@gmail.com.com>
2018-07-30 15:59:39 +0300

    Add Config file

---
+++ config.py
@@ -0,0 +1,40 @@
+import os
+
+"""
+    This file contains the different settings depending on the
+    application environment.
+"""
+
+
+class Config(object):
+    """Settings common to all configurations"""
+    SECRETE_KEY = os.urandom(24)
+    JWT_SECRET_KEY = "The lord is good."
+    DEBUG = False
+    TESTING = False
+    DATABASE_URI = 'postgres://admin:admin@localhost:5432/diary_db'
+
+
+class ProductionConfig(Config):
+    """Production configurations"""
+    DEBUG = False  # should be set to false always
+
+
+class DevelopmentConfig(Config):
+    """Development configurations"""
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    """Testing configurations"""
+    DEBUG = True
+    DATABASE_URI = 'postgres://admin:admin@localhost:5432/test_db'
+
+
+app_config = {
+    'production': ProductionConfig,
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+
+    'default': DevelopmentConfig
+}
