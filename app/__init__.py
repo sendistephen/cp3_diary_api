b592c84c889eecaa5851dd4a7d9c4c9f08d83697
SendiSteve <sendi.stev@gmail.com.com>
2018-07-30 17:33:33 +0300

    [chore] Add base testcase file

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
 from flask_restful import Api
 
 from config import app_config
+from app.views import RegisterResource
 
 
 def create_app(config_name):
@@ -16,3 +18,11 @@ def create_app(config_name):
 
 app = create_app(config_name='development')
 api = Api(app)
+
+app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
+jwt = JWTManager(app)
+
+# import config settings into the application
+app.config.from_object(app_config['development'])
+
+api.add_resource(RegisterResource, '/api/v2/auth/register')
---
+++ tests/base.py
@@ -0,0 +1,41 @@
+import json
+import unittest
+from app import app, app_config
+
+from dbconnection import Connection
+
+
+class BaseTestCase(unittest.TestCase):
+    @staticmethod
+    def create_app():
+        """
+         Create an instance of the app with the testing configurations
+        """
+        app.config.from_object(app_config['testing'])
+        return app
+
+    def setUp(self):
+        """Runs its code before every single test"""
+        # Initialize the test client
+        self.client = app.test_client(self)
+        app.config.from_object(app_config['testing'])
+        db = Connection()
+        db.create_tables()
+
+    def tearDown(self):
+        """Drop any stored data in the list after every single test runs"""
+        db = Connection()
+        db.trancate_table("users")
+        db.trancate_table("entries")
+
+    # ------------------------Register User--------------------------------#
+
+    def register_user(self, username, email, password):
+        """This methods registers the user"""
+        return self.client.post(
+            'api/v2/auth/register',
+            data=json.dumps(dict(
+                username=username,
+                email=email,
+                password=password
+            )), content_type='application/json')
