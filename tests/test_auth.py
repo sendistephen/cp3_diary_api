300808f179e1efc99e752b268df2108752f083c4
SendiSteve <sendi.stev@gmail.com.com>
2018-07-30 17:27:04 +0300

    [chore] Add test to check if user signup with invalid email

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -4,6 +4,7 @@ from tests.base import BaseTestCase
 
 
 class AuthTestCase(BaseTestCase):
+    # @unittest.skip('checking')
     def test_user_registers_successfully(self):
         """Tests user can register successfully through the api"""
         with self.client:
@@ -18,3 +19,13 @@ class AuthTestCase(BaseTestCase):
             data = json.loads(res.data.decode())
             self.assertEqual(res.status_code, 400)
             self.assertEqual(data.get('message'), 'Email already taken.')
+
+    def test_invalid_user_email_pattern(self):
+        """Tests user can not register with invalid email through the api"""
+        with self.client:
+            response = self.register_user(
+                'sendi', 'test1@test.', 'testpassword')
+            result = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertEqual(result.get('message'),
+                             'Please enter a valid email.')
