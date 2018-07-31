fb858f0507fc75f841ac0f375608a2d1dc15619d
SendiSteve <sendi.stev@gmail.com.com>
2018-07-31 15:12:09 +0300

    [chore] Add test to check if user submits a blank entry title

--- tests/test_entry.py
+++ tests/test_entry.py
@@ -10,11 +10,12 @@ class EntriesTestCase(BaseTestCase):
         with self.client:
             self.login_user("test@test.com", "password")
             response = self.create_entry(
-                'Meeting with the CEO Facebook',
+                'Meeting with the CEO Twitter',
                 'Discuss about new marketing strategies')
             result = json.loads(response.data.decode())
             self.assertEqual(response.status_code, 201)
-            self.assertEqual(result.get('message'), 'Entry recorded successfully.')
+            self.assertEqual(result.get('message'),
+                             'Entry recorded successfully.')
 
             res = self.create_entry(
                 'Meeting with the CEO Andela',
@@ -24,3 +25,14 @@ class EntriesTestCase(BaseTestCase):
             self.assertEqual(res.status_code, 400)
             self.assertEqual(result1.get(
                 'message'), 'Cannot have entries with the same title.')
+
+    def test_entry_title_is_black(self):
+        with self.client:
+            self.login_user("test@test.com", "password")
+            response = self.create_entry(
+                '',
+                'Discuss about new marketing strategies')
+            result = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertEqual(result.get('message'),
+                             'Please enter a valid entry.')
