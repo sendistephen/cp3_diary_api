a3a819c144ddd8e62f8292132aef4c889ddc5ade
SendiSteve <sendi.stev@gmail.com.com>
2018-08-02 17:56:57 +0300

    [chore] Add tests to test User object

---
+++ tests/test_model.py
@@ -0,0 +1,18 @@
+from app.models import User, Entry
+from tests.base import BaseTestCase
+
+
+class TestModelsTestcase(BaseTestCase):
+    def test_create_user(self):
+        user = User(user_id=1,
+                    username='Sendi',
+                    email='sendi.stev@gmail.com',
+                    password='password')
+        self.assertEqual(user.username, 'Sendi')
+
+    def test_create_entry(self):
+        entry = Entry(entry_id=1, user_id=1,
+                      title='I love dancing',
+                      notes='I want to learn dance lessons')
+        self.assertEqual(entry.title, 'I love dancing')
+    
