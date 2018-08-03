9ea0fac57ac8224e44e599f895b7e877d304abab
SendiSteve <sendi.stev@gmail.com.com>
2018-08-03 18:24:44 +0300

    [chore] Add doc string on methods in this file

--- tests/test_model.py
+++ tests/test_model.py
@@ -4,6 +4,7 @@ from tests.base import BaseTestCase
 
 class TestModelsTestcase(BaseTestCase):
     def test_create_user(self):
+        """Test if user can be created"""
         user = User(user_id=1,
                     username='Sendi',
                     email='sendi.stev@gmail.com',
@@ -11,6 +12,7 @@ class TestModelsTestcase(BaseTestCase):
         self.assertEqual(user.username, 'Sendi')
 
     def test_create_entry(self):
+        """Tests if entry can be created"""
         entry = Entry(entry_id=1, user_id=1,
                       title='I love dancing',
                       notes='I want to learn dance lessons')
